12dc5679f7eeffd5fc6422e4258406de891dc3ed
Stefan Jocic <jocicstefan91@gmail.com>
2022-01-03 17:47:03 +0100

    poruci

--- src/pasta.java
+++ src/pasta.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class pasta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
 		
 		String [] sastojak = {"Makarone","Spagete","Bolognese","Curetina",
 							"Govedja prsuta","Slanina","Piletina","4 sira",
@@ -12,7 +15,24 @@ public class pasta {
 		int [] cena = {50,60,120,120,140,100,100,100,80,50,80,80,80,50,50,20};
 		
 		String [] kupci = {"063111111","063222222","063333333","063444444","063555555"};
-
+		
+		
+		System.out.println("Izvolite");
+		String start = s.next();
+		
+		
+		while(poruci(start)) {
+			
+		}
+	}
+	
+	public static boolean poruci(String poruci) {
+		if (!poruci.equals("Poruci")) {
+			return false;
+		}else {
+			return true;
+		}
+		
 	}
 
 }
