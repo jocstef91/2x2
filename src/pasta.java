4b80046c36d5186a6867b6471004f68357c3aaf0
Stefan Jocic <jocicstefan91@gmail.com>
2022-01-03 17:37:59 +0100

    sastojci, cene, kupci

---
+++ src/pasta.java
@@ -0,0 +1,18 @@
+
+public class pasta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] sastojak = {"Makarone","Spagete","Bolognese","Curetina",
+							"Govedja prsuta","Slanina","Piletina","4 sira",
+							"Dimljeni sir","Parmezan","Pavlaka","Pesto sos",
+							"Napolitana","Povrce mix","Pecurke","Kutija"};
+		
+		int [] cena = {50,60,120,120,140,100,100,100,80,50,80,80,80,50,50,20};
+		
+		String [] kupci = {"063111111","063222222","063333333","063444444","063555555"};
+
+	}
+
+}
